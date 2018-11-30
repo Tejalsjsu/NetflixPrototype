f432534aca27aaac6ba4a00480efeca4ed7a2184
Aparna Dalal <34259886+aparnada7@users.noreply.github.com>
2018-11-30 09:59:19 -0800

    Files for admin navigation and dashboard
    Files for admin navigation and dashboard

---
+++ src/components/adminNavBar.js
@@ -0,0 +1,71 @@
+var React = require('react');
+var Link = require('react-router-dom').Link;
+var NavLink = require('react-router-dom').NavLink;
+var Button = require('react-bootstrap').Button;
+var cookie = require('react-cookies');
+var API = require("../api");
+
+let imgStyle = {height: '50px', width: '140px'};
+let btnStyle = { marginTop: '8px', marginRight: '4px'}
+
+
+function AdminNavBar() {
+    this.activateClass = (props) =>{
+        this.className= 'active'
+    }
+
+    this.logout = () =>{
+        var userId = cookie.load('userId');
+        cookie.remove('userId');
+        API.logout(userId)
+            .then((res) => {
+            console.log("status logout client" +[res.status]);
+            if (res.status === '201') {
+                console.log("successfull Log out");
+                cookie.remove('userId');
+                this.props.history.push('/login');
+            } else {
+                cookie.remove('userId');
+                this.props.history.push('/login');
+            }
+        });
+    }
+
+        return (
+           <div>
+                <nav className="navbar navbar-inverse navbar1">
+                    <div >
+                        <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div className="collapse navbar-collapse bar-item" id="myNavbar">
+                            <ul className="nav navbar-nav">
+                                <li onClick={this.activateClass} ><NavLink exact to="/adminAddMovie">Admin Dashboard</NavLink></li>
+                                <li onClick={this.activateClass} ><NavLink to="/myprojects">Movies</NavLink></li>
+                                <li onClick={this.activateClass} ><NavLink to="/addMoney">Subscription Reports</NavLink></li>
+
+                                <li onClick={this.activateClass} ><a href="/editprofile">User Reports</a></li>
+                                {/*className='active is-active'*/}
+                            </ul>
+
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><NavLink to="/profile">< span className="glyphicon glyphicon-log-in"></span> Admin Profile </NavLink></li>
+                                <li onClick={this.logout}><NavLink to="/login">< span className="glyphicon glyphicon-log-in"></span> Logout </NavLink></li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                </nav>
+
+                <div className="container">
+
+            </div>
+           </div>
+        )
+}
+
+module.exports = AdminNavBar;
